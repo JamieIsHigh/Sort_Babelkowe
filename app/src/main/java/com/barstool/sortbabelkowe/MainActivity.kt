97aeff20a49c0845a9d66ab465d36c3dc937d623
mihau11 <michi.leszczynski@gmaail.com>
2022-12-20 10:34:02 +0100

    Deklaracja

--- app/src/main/java/com/barstool/sortbabelkowe/MainActivity.kt
+++ app/src/main/java/com/barstool/sortbabelkowe/MainActivity.kt
@@ -2,10 +2,16 @@ package com.barstool.sortbabelkowe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    val textList = findViewById<TextView>(R.id.before)
+    val textSort = findViewById<TextView>(R.id.after)
+    val butList = findViewById<Button>(R.id.losuj)
+    val butSort = findViewById<Button>(R.id.sortuj)
 }
